301353788d00ad927db47e6f6fa8476583176403
JaxV9 <layanj.pro@gmail.com>
2026-02-19 11:31:17 +0100

    first state

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import Pyroscope from '@pyroscope/nodejs';
 
 const port = process.env.PORT || 5000;
 
@@ -19,6 +20,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+if (process.env.NODE_ENV === 'production') {
+  Pyroscope.init({
+    serverAddress: process.env.PYROSCOPE_SERVER_ADDRESS || 'http://localhost:4040',
+    appName: process.env.PYROSCOPE_APP_NAME || 'proshop-backend',
+  });
+  Pyroscope.start();
+}
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
